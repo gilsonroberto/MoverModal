0514fb8c3607d0b8d2118b8299f1a25ca54eaec7
unknown <gilsonroberto@gmail.com>
2020-01-20 15:04:14 -0300

    Teste Movimentar Modal Boostrap

---
+++ js/movimento.js
@@ -0,0 +1,10 @@
+
+        // Jquery draggable
+        $('.modal').draggable({
+            handle: ".modal-header"
+        });
+
+        function Real()
+        {
+        	$('.mov').css({"left": "0px", "top": "0px"});
+        }
